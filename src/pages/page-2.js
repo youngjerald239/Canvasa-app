b7638a8020cf38b564f694ba65920ed869915526
youngjerald239 <youngjerald239@gmail.com>
2021-11-21 23:55:53 -0600

    added canvas data

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,22 @@
 import React, {useLayoutEffect, useState} from "react"
 import { Link } from "gatsby"
-
+import rough from 'roughjs/bundled/rough.esm'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+
 
 const SecondPage = () => {
   useLayoutEffect(() => {
      const canvas = document.getElementById("canvas")
      const ctx = canvas.getContext("2d")
+
+
+    const roughCanvas = rough.canvas(canvas)
+    roughCanvas.rectangle(10, 10, 100, 100)
+
+    
+
    })
 
   return (
