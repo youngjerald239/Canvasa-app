a29890935cd8165e2bb222b584ea7b0a5eaed549
youngjerald239 <youngjerald239@gmail.com>
2021-11-21 23:46:25 -0600

    added canvas code

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,10 +7,12 @@ import Seo from "../components/seo"
 const SecondPage = () => {
   useLayoutEffect(() => {
      const canvas = document.getElementById("canvas")
+     const ctx = canvas.getContext("2d")
    })
 
   return (
   <Layout>
+    <canvas id='canvas' width={window.innerWidth} height={window.innerHeight}> Canvas </canvas>
     <Seo title="Page two" />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
